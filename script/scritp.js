cae0761f2e1041977a07d8f991d18dd3a1a0c3b0
Nikita Voskoboinikov <nikitavoskoboinikov2828@gmail.com>
2020-08-07 20:13:28 +0300

    get request and search fixed

---
+++ script/scritp.js
@@ -0,0 +1,50 @@
+const carTegs = `
+
+<img class="carImg" src="" alt="">
+<div class="carInfo">
+  <p class="infoBrand">Brand: </p>
+  <p class="infoModel">Model: </p>
+  <p class="infoYear">Year: </p>
+  <p class="infoKm">Mileage: </p>
+  <br />
+  <p class="infoDescription">
+    Description: <br />
+  </p>
+</div>
+<div class="wrapperCarBtn">
+  <button class="editBtn">Edit</button>
+  <button class="deleteBtn">Delete</button>
+</div>
+`;
+
+// const cars = document.querySelector(".cars")
+const BASE_URL = "https://node-server.vercel.app/cars"
+const carsList = document.querySelector(".cars")
+fetch(BASE_URL)
+.then((response) => response.json())
+.then((cars) => {
+    cars.forEach(car => {
+        const carr = document.createElement("div");
+        carr.classList.add("car");
+        carr.innerHTML = carTegs;
+
+        const carImg = carr.querySelector(".carImg");
+        const infoBrand = carr.querySelector(".infoBrand");
+        const infoModel = carr.querySelector(".infoModel");
+        const infoYear = carr.querySelector(".infoYear");
+        const infoKm = carr.querySelector(".infoKm");
+        const infoDescription = carr.querySelector(".infoDescription");
+
+        carImg.setAttribute("src", car.photo_link);
+        infoBrand.innerHTML = `Brand: ${car.brand}`;
+        infoModel.innerHTML = `Model: ${car.model}`;
+        infoYear.innerHTML = `Year: ${car.year}`;
+        infoKm.innerHTML = `Mileage : ${car.mileage}`;
+        infoDescription.innerHTML = `Description:<br> ${car.description}`;
+
+        console.log(carr)
+        carsList.appendChild(carr);
+    });
+})
+
+
